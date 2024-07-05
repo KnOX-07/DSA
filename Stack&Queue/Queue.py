7782957674604037ec258fa846de36b999c11ae7
_razzZ <114283758+KnOX-07@users.noreply.github.com>
2024-07-06 00:14:36 +0530

    Initial commit

--- Stack&Queue/Queue.py
+++ Stack&Queue/Queue.py
@@ -1,4 +1,4 @@
-'''class Queue:
+class Queue:
     def __init__(self):
         self.items = []
 
@@ -25,9 +25,9 @@
 
 queue = Queue()
 queue.is_empty()
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
 
 #popped_item = queue.dequeue()
 #print("Popped item: ", popped_item)
@@ -36,57 +36,4 @@ top_item = queue.peek()
 print("Top item of the stack: ", top_item)
 
 size = queue.size()
-print("Size of stack: ", size)'''
-
-class Queue:
-    def __init__(self, max_size):
-        self.l = [0] * max_size  # Initialize the list with zeros of size max_size
-        self.max = max_size  # Define the maximum size of the queue
-        self.front = -1  # Initialize front with -1 indicating the queue is empty
-        self.rear = -1  # Initialize rear with -1 indicating the queue is empty
-
-    def insertion(self, element):
-        if (self.rear + 1) % self.max == self.front:
-            print('Queue Overflow. Cannot insert to the queue.')
-            return
-        if self.front == -1:  # If the queue is initially empty
-            self.front = 0
-        self.rear = (self.rear + 1) % self.max  # Move rear to the next position
-        self.l[self.rear] = element  # Insert the new element
-        print(f'Inserted {element} to queue. Queue is now: {self.l}')
-
-    def deletion(self):
-        if self.front == -1:
-            print('Queue Underflow. Cannot delete from the queue.')
-            return None
-        element = self.l[self.front]  # Get the front element
-        if self.front == self.rear:  # If the queue is now empty
-            self.front = -1
-            self.rear = -1
-        else:
-            self.front = (self.front + 1) % self.max  # Move front to the next position
-        print(f'Deleted {element} from queue. Queue is now: {self.l}')
-        return element
-
-    def traverse(self):
-        if self.front == -1:
-            print('Queue is empty.')
-            return
-        print('Queue elements are:')
-        i = self.front
-        while True:
-            print(self.l[i], end=' ')
-            if i == self.rear:
-                break
-            i = (i + 1) % self.max
-        print()  # For a new line
-
-queue = Queue(5)  # Create a queue with maximum size 5
-
-queue.insertion(10)
-queue.insertion(20)
-queue.insertion(30)
-queue.traverse()
-
-queue.deletion()
-queue.traverse()
+print("Size of stack: ", size)
